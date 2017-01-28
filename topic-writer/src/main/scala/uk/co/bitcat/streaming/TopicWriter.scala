8a3916cdab75c982003007b9485cd8c992ac5e12
Daniel Cook <danielsteven.cook@gmail.com>
2017-01-28 23:38:42 +0000

    Kafka now receives some sample pollution data.

--- topic-writer/src/main/scala/uk/co/bitcat/streaming/TopicWriter.scala
+++ topic-writer/src/main/scala/uk/co/bitcat/streaming/TopicWriter.scala
@@ -1,7 +1,29 @@
 package uk.co.bitcat.streaming
 
+import java.util.Properties
+
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+
+import scala.io.Source
+
 object TopicWriter {
   def main(args: Array[String]) {
-    println("Hello, world!")
+    val source = Source.fromResource("readings.csv")
+    val lines : Iterator[String] = source.getLines
+
+    val props = new Properties
+    props.put("bootstrap.servers", "localhost:9092")
+    props.put("client.id", "Pollution")
+    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+
+    val producer = new KafkaProducer[String, String](props)
+    for (line <- lines) {
+      val data = new ProducerRecord[String, String]("pollution", line)
+      producer.send(data)
+    }
+
+    producer.close
+    source.close
   }
 }
